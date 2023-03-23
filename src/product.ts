636e51da0bb149c0d8a7ccb488846f56642ceef4
Kevin Williams <kevcoder@gmail.com>
2023-03-23 01:17:29 -0500

    added description property

--- src/product.ts
+++ src/product.ts
@@ -1,6 +1,6 @@
 export class Product {
     productId: string = '';
-    priceId: number = 0.00;
+    priceId: string = '';
     unitDescr: string = '';
     pricePerUnit: number = 0.00;
     quantity: number = 0.00;
@@ -8,23 +8,26 @@ export class Product {
     type: string = '';
     productName: string = '';
     metadata: {[key: string]: string} = {};
+    description: string = '';
 
     constructor(id: string,
         name: string,
-        priceId: number,
+        priceId: string,
         unitDescr: string,
+        description: string,
         pricePerUnit: number,
         quantity: number,
         imageUrl: string[],
         type: string,
         metadata: {}) {
         this.productId = id || '';
-        this.priceId = priceId || 0.00;
+        this.priceId = priceId || '';
         this.unitDescr = unitDescr || '';
         this.pricePerUnit = pricePerUnit || 0.00;
         this.quantity = quantity || 0.00;
         this.imageUrls = imageUrl || [];
         this.type = type || '';
         this.metadata = metadata || {};
+        this.description = description || '';
     }
 }
